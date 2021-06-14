69829908c4b0c58a7afbe4c4ca5b7cc99837b5d7
Ray suh <readingod@naver.com>
2021-06-14 15:20:48 +0900

    refactor: (#6) color -> labelcolor로 구조체 이름 변경

--- IssueTracker/IssueTracker/Model/Label.swift
+++ IssueTracker/IssueTracker/Model/Label.swift
@@ -3,12 +3,12 @@ import Foundation
 struct Label: Decodable {
     let id:Int
     let name:String
-    let color:Color
+    let color:LabelColor
     let description:String
     let checked:Bool
 }
 
-struct Color: Decodable {
+struct LabelColor: Decodable {
     let backgroundColorCode:String
     let textColorCode:String
 }
