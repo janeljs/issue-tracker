8821ec9f704be2ef3298b1d054a84b72c1607e5b
Ray suh <readingod@naver.com>
2021-06-14 18:17:08 +0900

    feat: (#6) Test 모듈 추가

---
+++ IssueTracker/IssueTrackerTests/MemoryStorageTest.swift
@@ -0,0 +1,15 @@
+import XCTest
+import RxSwift
+
+class MemoryStorageTest: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+    
+    func testCreateIssueMemoryStorage() {
+        let _ = IssueMemoryStorage()
+    }
+}
